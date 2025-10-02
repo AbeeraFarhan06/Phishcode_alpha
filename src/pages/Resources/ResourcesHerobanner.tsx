f5c68d2ef26c0997bb4dd7aeac38f0592b901683
Abeera Farhan <mecard2021@gmail.com>
2025-10-02 19:49:31 +0500

    Added seperate component for banner in Resource page

---
+++ src/pages/Resources/ResourcesHerobanner.tsx
@@ -0,0 +1,36 @@
+import { VStack, Box, Heading, Text } from "@chakra-ui/react";
+import Container from "../../components/Container";
+import contact_banner from "../../assets/png imgs/phishcode banner 3@5x.png";
+
+const ResourcesHerobanner = () => {
+  return (
+    <Box bgImage={contact_banner}
+      bgColor="#0E1726"
+      bgSize="cover"
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      height="auto"
+      width="100vw"
+      position="relative"
+      boxShadow="md"
+      _hover={{ boxShadow: "lg"}}
+      >
+      <Container fullWidth>
+        <VStack spacing={4} textAlign="center">
+          <Box py="4rem">
+            <Heading size="44px" fontWeight="semibold" textColor="white">
+              Security 101
+            </Heading>
+            <Text fontSize="16px" color="gray.600" mt={4} textColor="white">
+              Deepen your security knowledge and gain a fundamental understanding of
+              a variety of cybersecurity, identity, and compliance topics and best
+              practices.
+            </Text>
+          </Box>
+        </VStack>
+      </Container>
+    </Box>
+  );
+};
+
+export default ResourcesHerobanner;
